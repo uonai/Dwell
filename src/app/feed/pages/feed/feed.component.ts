eebc268955e6f15506aea834fbd828c34dd5ba8d
cs.mccaleb@gmail.com <cs.mccaleb@gmail.com>
2018-09-16 17:11:51 -0500

    Refactor Electron to simplify and remove Typescript dependencies.

--- src/app/feed/pages/feed/feed.component.ts
+++ src/app/feed/pages/feed/feed.component.ts
@@ -14,6 +14,7 @@ import '../../../rxjs-operators';
 export class FeedComponent implements OnInit {
 
   private feedUrl: string = 'http://www.ribbonfarm.com/feed/';
+  private feedUrl2: string = 'https://feedforall.com/sample-feed.xml';
   private feeds: any;
 
   constructor(
@@ -21,11 +22,14 @@ export class FeedComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.refreshFeed();
+    this.refreshFeed(this.feedUrl);
+    this.refreshFeed(this.feedUrl2);
+
   }
 
-  private refreshFeed(){
-    this.feedService.getFeedContent(this.feedUrl)
+  private refreshFeed(a: any){
+    const feedUrl = a;
+    this.feedService.getFeedContent(feedUrl)
     .subscribe(
       feed => this.feeds = feed.items,
       error => console.log(error));
